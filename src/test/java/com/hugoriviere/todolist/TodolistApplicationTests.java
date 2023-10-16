5c1572b5c1c8802fbbc1a3ab0a283627207ca2a0
hugoadventist <28987448+hugoadventist@users.noreply.github.com>
2023-10-16 18:59:13 -0300

    first commit do project

---
+++ src/test/java/com/hugoriviere/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hugoriviere.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
